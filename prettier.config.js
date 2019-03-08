bac654f09fa0f2d41950d732eeb3a46d306cf89a
Justin Neff <justin@gatewayapps.com>
2019-03-08 10:51:30 -0600

    setup typescript, tslint, prettier and yargs

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  printWidth: 250,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always'
+}
---
+++ src/cli/drift.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import yargs from 'yargs'
+
+export const argv = yargs.help().argv
