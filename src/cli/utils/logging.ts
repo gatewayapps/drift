64b40a50608bdd4381ab74480bc98104490b79c1
Justin Neff <justin@gatewayapps.com>
2019-03-25 15:51:20 -0500

    Added progress bar to cli output for tasks with multiple steps

--- src/cli/utils/logging.ts
+++ src/cli/utils/logging.ts
@@ -1,17 +1,18 @@
 import colors from 'colors'
+import { format } from 'date-fns'
 
 export const logger = {
   error(message?: any, ...optionalParams: any[]): void {
-    console.log(`${colors.red(getTimestamp())}: ${message}`, ...optionalParams)
+    console.log(`[${colors.red(getTimestamp())}]: ${message}`, ...optionalParams)
   },
   status(message?: any, ...optionalParams: any[]): void {
-    console.log(`${colors.blue(getTimestamp())}: ${message}`, ...optionalParams)
+    console.log(`[${colors.blue(getTimestamp())}]: ${message}`, ...optionalParams)
   },
   success(message?: any, ...optionalParams: any[]): void {
-    console.log(`${colors.green(getTimestamp())}: ${message}`, ...optionalParams)
+    console.log(`[${colors.green(getTimestamp())}]: ${message}`, ...optionalParams)
   }
 }
 
-function getTimestamp(): string {
-  return new Date().toISOString()
+export function getTimestamp(): string {
+  return format(new Date(), 'hh:mm:ss A')
 }
