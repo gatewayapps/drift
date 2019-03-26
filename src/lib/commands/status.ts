84c1a1d2dfb58d43266af91e6ad32dbfc69d6619
Justin Neff <justin@gatewayapps.com>
2019-03-26 17:05:22 -0500

    initial work on status command

---
+++ src/lib/commands/status.ts
@@ -0,0 +1,27 @@
+import { DatabasePublishStatus } from '../constants'
+import { DbContext } from '../DbContext'
+import { loadConfiguration } from '../DriftConfig'
+import { IDatabaseOptions } from '../interfaces/IDatabaseOptions'
+import { IStatusOptions } from '../interfaces/IStatusOptions'
+import { IStatusResult } from '../interfaces/IStatusResult'
+import { createDatabaseConnection } from '../utils/database'
+import { createMigrationHash, getMigrationsToApply, isMigrationRequired } from '../utils/migration'
+
+export async function status(options: IStatusOptions): Promise<IStatusResult> {
+  const config = await loadConfiguration(options.configFile)
+  const dbContext = await createContext(options.database)
+  const migrationHash = await createMigrationHash(config, options.database.provider)
+  if (!(await isMigrationRequired(migrationHash))) {
+    return {
+      migrations: [],
+      status: DatabasePublishStatus.UpToDate
+    }
+  }
+}
+
+async function createContext(dbOptions: IDatabaseOptions): Promise<DbContext> {
+  const dbConn = createDatabaseConnection(dbOptions)
+  await dbConn.authenticate()
+  const dbContext = new DbContext(dbConn)
+  return dbContext
+}
