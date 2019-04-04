5187e2dbe54afb6819091319cfaf8bb696e86729
Justin Neff <justin@gatewayapps.com>
2019-04-04 08:24:23 -0500

    Change how mssql objects are created vs altered

--- src/lib/dialects/mssql/MsSqlPublisher.ts
+++ src/lib/dialects/mssql/MsSqlPublisher.ts
@@ -4,28 +4,10 @@ import { Publisher } from '../abstract/Publisher'
 
 export class MsSqlPublisher extends Publisher {
   protected async preApplyDatabaseObject(dbContext: DbContext, databaseObject: IDatabaseObject): Promise<void> {
-    let createCmd = ''
     const fullQualifiedName = `[${databaseObject.schemaName}].[${databaseObject.objectName}]`
-    switch (databaseObject.type.toUpperCase()) {
-      case 'PROCEDURE':
-        createCmd = `CREATE PROCEDURE ${fullQualifiedName} AS SET NOCOUNT ON;`
-        break
-      case 'FUNCTION':
-        createCmd = `CREATE FUNCTION ${fullQualifiedName}() RETURNS INT AS BEGIN RETURN 1 END;`
-        break
-      case 'VIEW':
-        createCmd = `CREATE VIEW ${fullQualifiedName} AS SELECT 1 AS Val;`
-        break
+    const [result] = await dbContext.runRawQuery(`SELECT OBJECT_ID('${fullQualifiedName}') AS objectId`)
+    if (Array.isArray(result) && result.length > 0 && result[0].objectId !== null) {
+      databaseObject.text = databaseObject.text.replace(/^CREATE/i, 'ALTER')
     }
-    if (!createCmd) {
-      return
-    }
-    const ensureObjectCmd = `
-      IF OBJECT_ID('${fullQualifiedName}') IS NULL
-      BEGIN
-        EXECUTE('${createCmd}');
-      END
-    `
-    await dbContext.runRawQuery(ensureObjectCmd)
   }
 }
